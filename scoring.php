5c53fdf5b25e357d5855f742ca94dac224102bb3
Ryan <jewr@uci.edu>
2016-08-30 20:27:06 -0700

    Added Main Events for Scoring
    Signed-off-by: Ryan <jewr@uci.edu>

---
+++ scoring.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>SoCal Wushu Tournament Scoring</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <header>
+    <nav class="indigo" role="navigation">
+      <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo center">SoCal Wushu Tournament Scoring</a>
+
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+    <ul id="nav-mobile" class="side-nav fixed">
+      <li class="no-padding">
+        <ul class="collapsible collapsible-accordian">
+          <li class="bold">
+            <a class="collapsible-header waves-effect waves-pink">Contemporary</a>
+            <div class="collapsible-body">
+              <ul>
+                <li>
+                  <a href="longfist.html">Long Fist</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Southern Fist</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Broadsword</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Straightsword</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Southern Broadsword</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Staff</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Spear</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Southern Staff</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Other Barehand</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Other Weapon</a>
+                </li>
+              </ul>
+
+            </div>
+          </li>
+          <li class="bold">
+            <a class="collapsible-header waves-effect waves-pink">Traditional</a>
+            <div class="collapsible-body">
+
+              <ul>
+                <li>
+                  <a href="longfist.html">Northern Fist</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Southern Fist</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Short Weapon</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Long Weapon</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Other Barehand</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Other Weapon</a>
+                </li>
+              </ul>
+
+            </div>
+          </li>
+          <li class="bold">
+            <a class="collapsible-header waves-effect waves-pink">Internal</a>
+            <div class="collapsible-body">
+
+              <ul>
+                <li>
+                  <a href="longfist.html">Chen Style</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Yang Style</a>
+                </li>
+                <li>
+                  <a href="longfist.html">Taiji Weapon</a>
+                </li>
+              </ul>
+
+            </div>
+          </li>
+        </ul>
+
+      </li>
+    </ul>
+  </header>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
