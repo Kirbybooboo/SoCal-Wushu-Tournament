093b45391d362398a2594fb6aedc1cecd0578a4e
Kirbybooboo <e.ryanjew@gmail.com>
2016-10-05 14:11:45 -0700

    revised display to match scoring
    Signed-off-by: Kirbybooboo <e.ryanjew@gmail.com>

--- display.php
+++ display.php
@@ -4,17 +4,16 @@ session_start();
 
 include_once 'divisionFunctions.php';
 include_once 'processForm.php';
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<?php
+include_once 'navListFunctions.php';
 $user = 'wushuclub';
 $password = 'f4FreePhe';
 $link = mysqli_connect("localhost",$user,$password)  or die ("failed to connect to server !!");
 mysqli_select_db($link,"wushuclub");
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
@@ -24,10 +23,10 @@ mysqli_select_db($link,"wushuclub");
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
   <header>
+<!-- Top Navigation Bar -->
     <nav class="indigo" role="navigation" style="height: 144px">
       <div class="nav-wrapper container">
       <?php
@@ -45,64 +44,68 @@ mysqli_select_db($link,"wushuclub");
           }
         }
       ?>
+<!-- Competitor Dropdown -->
+        <ul class="right hide-on-med-and-down">
+          <ul id="competitorDropdown" class="dropdown-content">
+            <li><a href="#!">Empty</a></li>
+          </ul>
+          <li><a class='dropdown-button' data-beloworigin="true" href="#" data-activates='competitorDropdown' style="font-size: 16px;margin-top: 24px;">Competitor<i class="material-icons right">arrow_drop_down</i></a></li>
+          <ul id="dropdown2" class="dropdown-content">
+            <li><a>Empty</a></li>
+          </ul>
+        </ul>
       </div>
     </nav>
-    <ul id="nav-mobile" class="side-nav fixed">
+<!-- Events Side Nav-->
+    <div class="sideNavBar">
+      <ul id="nav-mobile" class="side-nav fixed">
+        <li class="logo">
+          <a id="logo-container" class="brand-logo" style="height: 144px">
+              <img class="responsive-img" src="img/front-logo.png">
+          </a>
+        </li>
+        <li class="no-padding">
+          <ul id="navEvent" class="collapsible collapsible-accordian">
+            <?php
+              getSideNavEvents();
+            ?>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </header>
+  <main>
+<!-- Division Side Nav -->
+      <ul id="slide-out" class="side-nav">  
       <li class="logo">
         <a id="logo-container" class="brand-logo" style="height: 144px">
             <img class="responsive-img" src="img/front-logo.png">
         </a>
       </li>
       <li class="no-padding">
-        <ul id="navEvent" class="collapsible collapsible-accordian">
+        <ul id="sideNavDivisions" class="collapsible collapsible-accordian">
           <?php
-            $styles=array(STYLE_CONTEMPORARY, STYLE_TRADITIONAL, STYLE_INTERNAL);
-            foreach($styles as $style)
-            {
-              if (!$_SESSION['eventId']) {$_SESSION['eventId'] = 1;}
-              echo '<li class="bold"><a class="collapsible-header waves-effect waves-pink">'.$style.'</a><div class="collapsible-body"><ul>';
-              $sql = 'SELECT `id`,`eventName` FROM `eventDefinition` WHERE `style`="'.$style.'"';
-              $result = mysqli_query($link, $sql);
-              while ($row = mysqli_fetch_assoc($result))
-              {
-                echo '<li><a class="waves-effect" href="#" onclick="setEventTitle('.$row['id'].');setDivisionList()">'.$row['eventName'].'</a></li>';
-              }
-              echo '</ul></div></li>';
-            }
+            getSideNavDivisions();
           ?>
         </ul>
       </li>
     </ul>
-  </header>
-
-  <main>
     <div class="container">
-
-      <br><br>
-      <a class='dropdown-button btn' href='#' data-activates='dropdown1' id='divisionButton'>Level/Gender/Age</a>
-      <ul id="dropdown1" class="dropdown-content">
-
-<?php
-      setDivisionListEventId();
-?>
-      </ul>
-      <a class='dropdown-button btn' href="#" data-activates='dropdown2'>Competitor</a>
-      <ul id="dropdown2" class="dropdown-content">
-<?php
-      echo '<li><a>Empty</a></li>';
-?>
-      </ul>
       <br>
+
 <?php
-      $retrieveCompetitor = "SELECT firstName, lastName FROM competitors WHERE id=".$_SESSION['competitorId'];
+      $retrieveCompetitor = "SELECT id, firstName, lastName, gender, birthDate, level FROM competitors WHERE id=".$_SESSION['competitorId'];
       $result = mysqli_query($link, $retrieveCompetitor);
       $row = mysqli_fetch_assoc($result);
-      echo "<h1 class='header' id='competitorName'>".$row['firstName']." ".$row['lastName']."</h1>";
-      $retrieveScore = 'SELECT scoreTotal FROM eventScoring WHERE competitorId='.$_SESSION['competitorId'].' AND eventId='.$_SESSION['eventId'];
-      $result = mysqli_query($link, $retrieveScore);
-      $row=mysqli_fetch_assoc($result);
-      echo '<h1 class="header">'.$row['scoreTotal'].'</h1>';
-?>
+      if (mysqli_num_rows($result) > 0)
+      {
+        echo "<h1 class='header' id='competitorName'>".$row['firstName']." ".$row['lastName']."</h1>";
+      }
+      else
+      {
+        echo '<h1 class="header" id="competitorName">Competitor Name</h1>';
+      }
+?> 
 
     </div>
   </main>
@@ -110,7 +113,7 @@ mysqli_select_db($link,"wushuclub");
   <script src="js/scripts.js"></script>
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+  <script>$(".button-collapse").sideNav({closeOnClick: true});</script>
 
   </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 function setEventTitle(id)
 {
-	$("#eventTitle").load("setEventTitle.php?id="+id);
+	$('#eventTitle').load('setEventTitle.php?id='+id);
 }
 
 function getSideNavDivisions()
 {
-	$("#sideNavDivisions").load("setDivisionList.php");
+	$('#sideNavDivisions').load('setDivisionList.php');
 }
 
 function setCompetitorList(eventId, level, gender, age)
 {
-	$("#competitorDropdown").load("setCompetitorList.php?eventId="+eventId+"&level="+level+"&gender="+gender+"&age="+age);
+	$('#competitorDropdown').load('setCompetitorList.php?eventId='+eventId+'&level='+level+'&gender='+gender+'&age='+age);
 }
 
 function resetCompetitorList()
 {
-	$("#competitorDropdown").load("resetCompetitorList.php");
+	$('#competitorDropdown').load('resetCompetitorList.php');
 }
 
 function resetCompetitor()
 {
-	$("#competitorName").load("resetCompetitor.php");
+	$('#competitorName').load('resetCompetitor.php');
 }
 
 
 function setCompetitor(id)
 {
-	$("#competitorName").load("setCompetitor.php?id="+id);
+	$('#competitorName').load('setCompetitor.php?id='+id);
 }
 
 function submitScore(type)
 {
-	$("#scoreForm").submit(function(e)
+	$('#scoreForm').submit(function(e)
 	{
     $.ajax({
-           type: "POST",
-           url: "AJAXprocessForm.php?type="+type,
-           data: $("#scoreForm").serialize(),
+           type: 'POST',
+           url: 'AJAXprocessForm.php?type='+type,
+           data: $('#scoreForm').serialize(),
            success: function(data)
            {
                $('#submitSuccess').openModal();
@@ -48,27 +48,27 @@ function submitScore(type)
 
 function setJudge(judge)
 {
-	$("#scoreForm").load("setJudge.php?judgeId="+judge);
+	$('#scoreForm').load('setJudge.php?judgeId='+judge);
 	Materialize.toast('Now Judge '+judge, 4000);
 }
 
 function setJudgeDropdownTitle(judge)
 {
-	$("#judgeDropdownTitle").load("setJudgeDropDownTitle.php?judge="+judge);
+	$('#judgeDropdownTitle').load('setJudgeDropDownTitle.php?judge='+judge);
 }
 
 function setHeadJudge()
 {
-	$("#scoreForm").load("setHeadJudge.php");
+	$('#scoreForm').load('setHeadJudge.php');
 	Materialize.toast('Now Head Judge', 4000)
 }
 
 function refreshTable()
 {
-	$("#tableBody").load("refreshTable.php");
+	$('#tableBody').load('refreshTable.php');
 }
 
 function refreshFinalScore()
 {
-	$("#totalScore").load("refreshFinalScore.php");
+	$('#totalScore').load('refreshFinalScore.php');
 }
